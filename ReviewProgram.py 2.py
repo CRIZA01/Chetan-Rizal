923d3190748139c1c3b4dcc85f03bd46758727c8
Chetan Rizal <chetan.rizal@stu.jefferson.kyschools.us>
2018-03-12 08:37:44 -0400

    first commit

---
+++ ReviewProgram.py 2.py
@@ -0,0 +1,10 @@
+#chetan_rizal-assign01.py
+media = input("what is the media ? ")
+Titles = input("what is the titles of your media? ")
+generes = input("what is the genres? ")
+years = input("which year it's realease? ")
+Description = input("what is description? ")
+Rating = float(100)
+
+new_list = [media,Titles,generes,years,Description,Rating]
+print (media,Titles,generes,years,Description,Rating)
