432bc918e84e0d3c90b44754279ce180011d68c6
Chetan Rizal <chetan.rizal@stu.jefferson.kyschools.us>
2018-03-16 08:19:07 -0400

    firstcommit dictinary assign

---
+++ dict_assignment.py
@@ -0,0 +1,16 @@
+ct_rizal = {
+    'accommodat': " provide lodging or sufficient space for",
+    
+    'definition': "the act of defining, or of making something definite, distinct, or clear:",
+    
+    'dekko': "a look or glance.",
+    
+    'reminisce': "to recall past experiences, events, etc.",
+    
+    'humility': "a modest or low view of one's own importance; humbleness.",
+    
+    'jurisdiction': "the official power to make legal decisions and judgments",
+    
+    'karma': "he sum of a person's actions in this and previous states of existence, viewed as deciding their fate in future existences.",
+    
+    }
